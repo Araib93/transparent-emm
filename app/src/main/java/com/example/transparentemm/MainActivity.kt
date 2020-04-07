dd338cd64fcd69d4a1e9af971f7175eb20b69f6f
Araib Shafiq <>
2020-04-07 17:07:02 +0500

    - Added wallpaper support

--- app/src/main/java/com/example/transparentemm/MainActivity.kt
+++ app/src/main/java/com/example/transparentemm/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.transparentemm
 
+import android.Manifest
+import android.app.WallpaperManager
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_READ_STORAGE = 100
 
     private val flags = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -16,14 +23,52 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         window.addFlags(
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-                        or WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
-                        or WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                    or WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                    or WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
         )
 
 //        window.decorView.systemUiVisibility = flags
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkStoragePermission()?.let { setWallpaper() }
+    }
+
+    private fun checkStoragePermission(): Any? {
+        return if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) true else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                REQUEST_READ_STORAGE
+            )
+            null
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_READ_STORAGE -> {
+                if (permissions[0] == Manifest.permission.READ_EXTERNAL_STORAGE
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    setWallpaper()
+                }
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    private fun setWallpaper() {
+        container.background = WallpaperManager.getInstance(this).drawable
     }
 
     override fun onBackPressed() {}
